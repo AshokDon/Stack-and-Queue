17880ffb8852a6b87ddf14b46c163827971508db
Ashok <68994450+AshokDon@users.noreply.github.com>
2021-05-20 10:17:41 +0530

    Add files via upload

---
+++ Balanced_chacker.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    string s;
+	    cin>>s; 
+	    stack<char>st;
+	    int fl=1;
+	    for(auto it:s)
+	    {
+	        if(it =='[' || it=='{' || it=='(')
+	        {
+	            st.push(it);
+	        }
+	        else{
+	            if(it==']' && !st.empty() && st.top()=='[')
+	            {
+	                st.pop();
+	            }
+	            else if(it=='}' && !st.empty() && st.top()=='{')
+	            {
+	                st.pop();
+	            }
+	            else if(it==')' && !st.empty() && st.top()=='(')
+	            {
+	                st.pop();
+	            }
+	            else{
+	                fl=0;
+	            }
+	        }
+	    }
+	    if(st.size()!=0 || fl==0)
+	    {
+	        cout<<"No";
+	    }
+	    else{
+	        cout<<"Yes";
+	    }
+	}
+	return 0;
+}
