569b3a347bd89bed2d1e9eb1d43d9c05feeddcbb
Ashok <68994450+AshokDon@users.noreply.github.com>
2023-04-10 15:52:02 +0530

    Update stack.c

--- stack.c
+++ stack.c
@@ -33,6 +33,7 @@ int Isempty(){
 }
 int size(){
     return top+1;
+    
 }
 int Top(){
     if(top==EMPTY)return STACK_EMPTY;
@@ -51,6 +52,91 @@ int main(){
     }
 
 }
+//2nd method using struct
+/*  /////////////////////////////////////////////////////////////////////. */
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
+void TakeInput(){
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+}
+struct Stack{
+	int *arr;
+	int top;
+	int cap;
+};
+typedef struct Stack stack;
+//creat an stack with given capacity
+stack *Creat_A_Stack(int capacity){
+	stack *ss = (stack*)malloc(sizeof(stack));
+	ss->cap = capacity;
+	ss->top = -1;
+	ss->arr=(int*)malloc(ss->cap * sizeof(int));
+	return ss;
+}
+//to add an give data into stack
+void Push(struct Stack *S, int val){
+	if(S->top == S->cap-1)return;
+	S->top++;
+	S->arr[S->top]=val;
+}
+
+int pop(stack *s1){
+	if(s1->top==-1){
+		printf("stack is empty");
+		return 0;
+	}
+	int res = s1->arr[s1->top];
+	s1->top--;
+	return res;
+
+}
+int Top(stack *s1){
+	if(s1->top==-1){
+		printf("stack is empty");
+		return 0;
+	}
+	int res = s1->arr[s1->top];
+	
+	return res;
+
+}
+int empty(stack *s1){
+	return s1->top==-1;
+}
+void Display(stack *s1){
+	while(!empty(s1)){
+		printf("%d\n",s1->arr[s1->top]);
+		s1->top--;
+	}
+}
+int main(){
+	TakeInput();
+	stack *s1 = Creat_A_Stack(5);
+	Push(s1,10);
+	Push(s1,20);
+	Push(s1,30);
+	//pop(s1);//-->s1 top delet
+	Top(s1);// -->s1 top return 
+//size(s1);// s1 size
+	Display(s1);//print all data in s1
+	stack *s2 = Creat_A_Stack(5);
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
